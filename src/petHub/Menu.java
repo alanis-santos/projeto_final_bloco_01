3afadbea4d14f9058782d1098f9e09d39046766e
Alanis Santos <alanisosantos@outlook.com>
2026-05-07 14:05:08 -0300

    :adhesive_bandage: Ajustando Menu

--- src/petHub/Menu.java
+++ src/petHub/Menu.java
@@ -3,14 +3,11 @@ package petHub;
 import java.util.Scanner;
 
 import petHub.model.Alimentos;
-import petHub.model.Produto;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
-		Produto pp = new Produto("Bolinha", "Smurf", 5f);
-		pp.visualizar();
 
 		Alimentos cc = new Alimentos("Ração para Gatos", "Whiskas", 107f, "Salmão", 10.1f);
 		cc.visualizar();
